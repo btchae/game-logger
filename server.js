332b60837590261015ebefd8e0ba4f38c5970f5c
btchae <bchae@nyu.edu>
2016-07-16 14:50:47 -0400

    adding controllers and models

---
+++ server.js
@@ -0,0 +1,34 @@
+//Requirements
+var express = require('express'); 
+var app = express();
+var port = process.env.PORT || 3000;
+var passport = require('passport');
+var flash = require('connect-flash'); // store and retrieve messages in session store
+var morgan = require('morgan'); //logger
+var cookieParser = require('cookieParser'); // parse cookies
+var bodyParser = require('body-parser'); // parse posts
+var ssession = require('express-session');// session middleware
+require('./config/passport')(passport); // pass passport for configuration
+
+//Middleware
+app.use(morgan('dev'));// log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser());// get information from html forms
+
+//this Middleware is required for passport
+app.use(session({ secret: 'zomaareenstukjetekstDatjenietzomaarbedenkt' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+// app.set('view engine', 'ejs');  I might use ejs, or I might make it single page.
+
+//Controllers
+// var userController = require('./controllers/users.js');
+require('./controllers/users.js')(app, passport);
+require('./controllers/games.js')(app, passport);
+
+//Listen
+app.listen(port);
+console.log('=============================');
+console.log('The name of our band is PORT: ' + port);
+console.log('=============================');
