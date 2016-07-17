2ec98203645c7667a2b2d554de8fb0e63661362f
btchae <bchae@nyu.edu>
2016-07-17 19:07:34 -0400

    adding game model

---
+++ models/games.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var Sequelize = require("sequelize");
+var db = process.env.DATABASE_URI || "postgres://localhost/game_logger";
+var connection = new Sequelize(db);
+var bcrypt = require('bcryptjs');
+var crypto = require('crypto');
+
+var Game = connection.define('games', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false 
+  },
+  image: {
+    type: Sequelize.TEXT, 
+    allowNull: true
+  },
+  deck: {
+    type: Sequelize.TEXT,
+    allowNull: true
+  }, 
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: true
+  },
+  platforms: {
+    type: Sequelize.ARRAY,
+    allowNull: true
+  },
+  ratings: {
+    type: Sequelize.ARRAY,
+    allowNull: true
+  }
+});
+
+var table = function() {
+    Game.sync().then(function() {
+    //Table created
+    console.log("Game Table Created");
+  })
+}
+
+module.exports = {
+  model: Game,
+  table: table
+};
